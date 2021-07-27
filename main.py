be86ff9afff607e8b662106034b2c6bcc6341da3
AntiSune101 <75425762+Antisune101@users.noreply.github.com>
2021-07-27 10:33:08 +0000

    Some Minor Bug fixes

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import discord
 from discord.ext import commands
 import random
-import asyncio
 
 client = discord.Client()
 client = commands.Bot(command_prefix='.')
 
+games = {}
+
 ##variables
 class game(object):
 	def __init__(self, player, channel):
-		self.board = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' ', 9: ' '}
+		self.board = {1: '#', 2: '#', 3: '#', 4: '#', 5: '#', 6: '#', 7: '#', 8: '#', 9: '#'}
 		self.player_token = 'O'
 		self.bot_token = 'X'
 		self.botStarts = False
 		self.gameOver = False
 		self.player = player
 		self.channel = channel
+		self.playerTurn = False
 
 	async def printBoard(self):
 		board_message = '\n' + self.board[1] + '|' + self.board[2] + '|' + self.board[3] + '\n' + self.board[4] + '|' + self.board[5] + '|' + self.board[6] + '\n' + self.board[7] + '|' + self.board[8] + '|' + self.board[9]
-
 		await self.channel.send(board_message)
 
 	def spaceIsFree(self, position):
-		if self.board[position] == '#':
+		if self.board[int(position)] == '#':
 			return True
 		else:
 			return False
@@ -39,7 +40,6 @@ class game(object):
 					self.startGame(True, 'draw')
 				else:
 					exit()
-
 			if self.checkForWin():
 				self.gameOver = False
 				if letter == 'X':
@@ -53,10 +53,13 @@ class game(object):
 					if self.requestRestart():
 						self.startGame(True, 'player')
 			return
+		elif self.playerTurn:
+			await self.channel.send("Position is not Available")
+		
 		else:
 			print("Can't insert there!")
 			position = int(input("Please enter new position:  "))
-			self.insertLetter(letter, position)
+			await self.insertLetter(letter, position)
 		return
 
 	def checkForWin(self):
@@ -101,32 +104,38 @@ class game(object):
 
 	def checkDraw(self):
 		for key in self.board.keys():
-			if (self.board[key] == ' '):
+			if (self.board[key] == '#'):
 				return False
 		return True
 
-	async def playerMove(self):
-		position = 0
-		while not position in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-			position = int(input("Enter the position for 'O':  "))
-		await self.insertLetter(self.player_token, position)
-		return
+	async def playerMove(self, position:int):
+		if self.playerTurn:
+			if position  in range(1, 9):
+				await self.channel.send("Invalid Position")
+				print("Hello")
+				return
+			await self.insertLetter(self.player_token, position)
+			self.playerTurn = False
+			return
+		else:
+			await self.channel.send("Its not your Turn {}".format(self.player))
 
 	async def compMove(self):
 		bestScore = -800
 		bestMove = 0
 		for key in self.board.keys():
-			if (self.board[key] == ' '):
+			if (self.board[key] == '#'):
 				self.board[key] = self.bot_token
 				score = self.minimax(self.board, 0, False)
-				self.board[key] = ' '
+				self.board[key] = '#'
 				if (score > bestScore):
 					bestScore = score
 					bestMove = key
 
 		await self.insertLetter(self.bot_token, bestMove)
+		self.playerTurn = True
+		await self.channel.send("Bot plays {}".format(bestMove))
 		return
-
 
 	def minimax(self, board, depth, isMaximizing):
 		if (self.checkWhichMarkWon(self.bot_token)):
@@ -158,37 +167,46 @@ class game(object):
 						bestScore = score
 			return bestScore
 
-	async def startGame(self, isRematch, winner):
-		self.gameOver = False
-		self.board = {1: '#', 2: '#', 3: '#', 4: '#', 5: '#', 6: '#', 7: '#', 8: '#', 9: '#'}
-		if isRematch:
-			if (winner == 'player'):
-				botStarts = False
-			if (winner == 'bot'):
-				botStarts = True
-			if (winner == 'draw'):
-				botStarts = bool(random.randint(0, 1))
-		else:
-			botStarts = bool(random.randint(0, 1))
+	async def startGame(self):
+		botStarts = bool(random.randint(0, 1))
 		if botStarts:
-			while not self.gameOver:
-				await self.compMove()
-				await self.playerMove()
+				self.playerTurn = False
 		else:
+				self.playerTurn = True
+		
+		if not botStarts:
+			await self.channel.send("{} goes first".format(self.player))
 			await self.printBoard()
-			while not self.gameOver:
-				await self.playerMove()
+		else:
+			await self.channel.send("Bot goes first againts {}".format(self.player))
+		while not self.gameOver:
+			while not self.playerTurn:
 				await self.compMove()
 
 @client.command()
+async def play(ctx, position):
+	print("pls Print")
+	if (ctx.message.author in games):
+		print("Me First")
+		await games[ctx.message.author].playerMove(int(position))
+		print("hello")
+		return
+	else:
+		await ctx.send("You are not in a game use .start to start one")
+		print("something")
+		return
+
+@client.command()
 async def start(ctx):
-	session = game(ctx.message.author, ctx.channel)
-	await session.startGame(False, 'player')
-	await session.printBoard()
+	if (ctx.message.author not in games):
+		games[ctx.message.author] = game(ctx.message.author, ctx.channel)
+		await games[ctx.message.author].startGame()
+		return
+	else:
+		await ctx.send("You are already in a game use .stop to stop the current game")
 
 @client.event
 async def on_ready():
     print("logged in")
-
 
 client.run("ODIyMDgwODQ0ODk0MDQ0MTcx.YFNEcg.KrIGkmAIBQeifqWbi_JP5L3v0oM")
